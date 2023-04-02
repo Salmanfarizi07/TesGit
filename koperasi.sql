5aadc312eb6e5aa7a5aa64c6891a79125c5b9c09
SAFANA SALSABILA <90315473+safanabilla28@users.noreply.github.com>
2023-04-02 22:38:06 +0700

    Delete koperasi.sql

--- koperasi.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE peminjaman (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  nama_peminjam varchar(255) NOT NULL,
-  tanggal_pinjam date NOT NULL,
-  tanggal_kembali date NOT NULL,
-  jumlah_pinjaman int(11) NOT NULL,
-  PRIMARY KEY (id)
-);
