928b8d10924b13b3121a2bdb26d03f911f93f1f7
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-04-03 04:38:38 +0800

    allatifatul commit 1

--- Layanan/routes/web.php
+++ Layanan/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/Surat', function () {
     return view('Surat');
@@ -61,3 +59,32 @@ Route::post('/Saham/store','App\Http\Controllers\SahamController@store');
 Route::get('/Saham/edit/{Id_Saham}','App\Http\Controllers\SahamController@edit');
 Route::post('/Saham/update','App\Http\Controllers\SahamController@update');
 Route::get('/Saham/hapus/{Id_Saham}','App\Http\Controllers\SahamController@hapus');
+
+/**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+    Route::resource('/user', \App\Http\Controllers\UserController::class);
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
+
