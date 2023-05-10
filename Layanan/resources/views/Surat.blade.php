29637271ce14fc87d3b045f5d61ba5691e511e11
Xenzy123 <daffaraifr@students.unnes.ac.id>
2023-05-10 21:14:12 +0700

    ubah tampilan tabel surat

--- Layanan/resources/views/Surat.blade.php
+++ Layanan/resources/views/Surat.blade.php
@@ -7,8 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-	<button><a href="/tambah">Tambah</a></button>
-<table border="1">
+	<h3 class="h3">
+	Surat
+	</h3>
+<table border="1" class="tabel">
 		<tr>
 			<th>Id_Surat</th>
 			<th>Id_User</th>
@@ -36,5 +38,60 @@
 		</tr>
 		@endforeach
 	</table>
+	<button class="tambah"><a href="/tambahSurat">Tambah Surat</a></button>
+	<style>
+		.tabel {
+			border-collapse: collapse;
+			margin: 25px 0;
+			font-size: 0.9em;
+			font-family: sans-serif;
+			min-width: 400px;
+			}
+
+		.tabel thead tr {
+			background-color: #009879;
+			color: #ffffff;
+			text-align: left;
+			}
+
+		.tabel th,
+		.tabel td {
+			padding: 12px 15px;
+		}
+
+		.tabel tbody tr {
+    		border-bottom: 1px solid #dddddd;
+		}
+
+		.tabel tbody tr:nth-of-type(even) {
+			background-color: #f3f3f3;
+		}
+
+		.tabel tbody tr:last-of-type {
+			border-bottom: 2px solid #009879;
+		}
+
+		.tabel tbody tr.active-row {
+			font-weight: bold;
+			color: #009879;
+		}
+		
+		.tambah {
+			background-color: #91c495; 
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+
+		.h3 {
+			font-weight: bold;
+			font-family: sans-serif;
+		}
+
+	</style>
 </body>
 </html>
