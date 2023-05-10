13a6bcea9b7148f42db94f5df53cf98d9368b528
Xenzy123 <daffaraifr@students.unnes.ac.id>
2023-05-10 21:26:42 +0700

    tambah surat

--- Layanan/routes/web.php
+++ Layanan/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/Surat', function () {
     return view('Surat');
 });
 
-Route::get('/tambah', function () {
+Route::get('/tambahSurat', function () {
     return view('tambah');
 });
 
