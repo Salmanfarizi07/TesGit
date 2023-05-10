4c391211489cc65ffd612defe0048f8619a6693c
SAFANA SALSABILA <90315473+safanabilla28@users.noreply.github.com>
2023-05-11 00:01:25 +0700

    Peminjaman.blade.php

--- Layanan/resources/views/Peminjaman.blade.php
+++ Layanan/resources/views/Peminjaman.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    </head>
-    <body>
-    <h3 class="h3">
+</head>
+<body>
+	<h3 class="h3">
 	Peminjaman
-    </h3>
+	</h3>
 <table border="1" class="tabel">
 		<tr>
-			<th>Id_Peminjaman</th>
+		<th>Id_Peminjaman</th>
 			<th>Id_User</th>
             <th>Jenis_Peminjaman</th>
             <th>Desc_Peminjaman</th>
@@ -21,9 +21,9 @@
             <th>Status_Peminjaman</th>
 			<th>Opsi</th>
 		</tr>
-        @foreach($Peminjaman as $p)
+		@foreach($Peminjaman as $p)
 		<tr>
-			<td>{{ $p->Id_Peminjaman }}</td>
+		<td>{{ $p->Id_Peminjaman }}</td>
 			<td>{{ $p->Id_User }}</td>
 			<td>{{ $p->Jenis_Peminjaman }}</td>
 			<td>{{ $p->Desc_Peminjaman }}</td>
@@ -31,12 +31,12 @@
             <td>{{ $p->Jumlah_Peminjaman }}</td>
             <td>{{ $p->Status_Peminjaman }}</td>
 			<td>
-            <a href="/Peminjaman/edit/{{ $p->Id_Peminjaman }}">Edit</a>
+				<a href="/Peminjaman/edit/{{ $p->Id_Peminjaman }}">Edit</a>
 				|
 				<a href="/Peminjaman/hapus/{{ $p->Id_Peminjaman }}">Hapus</a>
 			</td>
 		</tr>
-        @endforeach
+		@endforeach
 	</table>
 	<button class="tambah"><a href="/tambahPeminjaman">Tambah Peminjaman</a></button>
 	<style>
@@ -94,5 +94,6 @@
 
 
 	</style>
-    </body>
+</body>
+
 </html>
