209688b58174046134df725639c968b7561277d9
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-05-11 13:37:58 +0700

    edit homepage

--- Layanan/resources/views/home/index.blade.php
+++ Layanan/resources/views/home/index.blade.php
@@ -46,8 +46,8 @@
     @endauth
 
     @guest
-    <h1>Homepage</h1>
-    <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+    <h1>Layanan Koperasi Mahasiswa</h1>
+    <p class="lead">Register atau Login untuk menampilkan homepage Layanan Koperasi Mahasiswa.</p>
     @endguest
 </div>
 
