e6f5f33a73e82ed3e98d656f26b9cdc64f25c0d1
jihara <94060347+jihara24@users.noreply.github.com>
2023-05-24 23:48:08 +0700

    Update tambahpengajuan.blade.php

--- Layanan/resources/views/tambahpengajuan.blade.php
+++ Layanan/resources/views/tambahpengajuan.blade.php
@@ -15,7 +15,7 @@
 	<form action="/pengajuan/input" method="post">
 		{{csrf_field()}}
 		ID User <input type="number" name="id_user" required="required"> <br/>
-		Jenis Pengajuan <input type="text" name="jenis_pengajun" required="required"> <br/>
+		Jenis Pengajuan <input type="text" name="jenis_pengajuan" required="required"> <br/>
 		Tanggal Masuk <input type="date" name="tgl_awal" required="required"> <br/>
 		Jumlah Pengajuan <input type="number" name="jumlah_pengajuan" required="required"> <br/>
 		<input type="submit" value="Simpan Data">
