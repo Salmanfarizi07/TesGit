df987e7ac094e9afb58f8c224c5677ff2fc8afea
jihara <94060347+jihara24@users.noreply.github.com>
2023-05-24 16:48:15 +0700

    Create tambahpengajuan.blade.php

---
+++ Layanan/resources/views/tambahpengajuan.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah Pengajuan</title>
+</head>
+<body>
+ 
+		<h2>Data Pengajuan</h3>
+ 
+	<a href="/pengajuan"> Kembali</a>
+	
+	<br/>
+	<br/>
+ 
+	<form action="/pengajuan/input" method="post">
+		{{csrf_field()}}
+		ID User <input type="number" name="id_user" required="required"> <br/>
+		Jenis Pengajuan <input type="text" name="jenis_pengajun" required="required"> <br/>
+		Tanggal Masuk <input type="date" name="tgl_awal" required="required"> <br/>
+		Jumlah Pengajuan <input type="number" name="jumlah_pengajuan" required="required"> <br/>
+		<input type="submit" value="Simpan Data">
+	</form>
+ 
+</body>
+</html>
