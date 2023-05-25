e05a56b6d0d8ce468ab14c09656af100051012a5
LAPTOP-L5EUQFA3\SAFANA SALSABILA <safanasalsabilla10@students.unnes.ac.id>
2023-05-25 12:27:34 +0700

    Update edit jasa

--- Layanan/resources/views/editJasa.blade.php
+++ Layanan/resources/views/editJasa.blade.php
@@ -8,32 +8,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h2 class="judul"> Edit Pembayaran </h2>
+    <h2 class="judul"> Edit Jasa </h2>
     <a href="/Pembayaran" class="kembali">Kembali</a>
 @foreach($Jasa as $p)
 <form action="/Jasa/update" method="post">
 	{{ csrf_field() }}
 	<div class="form-group">
 		<label for="Nama_Jasa ">Nama_Jasa</label>
-		<input type="text" required="required" name="Jenis_Pembayaran" id="jenis_pembayaran" value="{{ $p->Jenis_Pembayaran }}">
+		<input type="text" required="required" name="Nama_Jasa" id="Nama_Jasa" value="{{ $p->Nama_Jasa }}">
 	</div>
 	<div class="form-group">
 		<label for="Jenis_Jasa">Jenis_Jasa</label>
-		<input type="text" required="required" name="Metode_Pembayaran" id="metode_pembayaran" value="{{ $p->Metode_Pembayaran }}">
+		<input type="text" required="required" name="Jenis_Jasa" id="Jenis_Jasa" value="{{ $p->Jenis_Jasa }}">
 	</div>
 	<div class="form-group">
 		<label for="Desc_Jasa ">Desc_Jasa</label>
-		<input type="text" required="required" name="Status_Pembayaran" id="status_pembayaran" value="{{ $p->Status_Pembayaran }}">
+		<input type="text" required="required" name="Desc_jasa" id="Desc_jasa" value="{{ $p->Desc_Jasa }}">
 	</div>
     <div class="form-group">
 		<label for="Tgl_Pengajuan ">Tgl_Pengajuan</label>
-		<input type="text" required="required" name="Status_Pembayaran" id="status_pembayaran" value="{{ $p->Status_Pembayaran }}">
+		<input type="text" required="required" name="Tgl_Pengajuan" id="Tgl_pengajuan" value="{{ $p->Tgl_Pengajuan }}">
 	</div>
     <div class="form-group">
 		<label for="Status_Jasa ">Status_Jasa</label>
-		<input type="text" required="required" name="Status_Pembayaran" id="status_pembayaran" value="{{ $p->Status_Pembayaran }}">
+		<input type="text" required="required" name="Status_Jasa" id="status_jasa" value="{{ $p->Status_Jasa }}">
 	</div>
-	<input type="hidden" name="Id_Pembayaran" value="{{ $p->Id_Pembayaran }}"> <br/>
+	<input type="hidden" name="Id_Jasa" value="{{ $p->Id_Jasa }}"> <br/>
 	<input type="submit" value="Simpan Data" class="simpan">
 </form>
 @endforeach
