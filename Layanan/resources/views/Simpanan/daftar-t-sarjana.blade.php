d20fc8ba839505e7dda5f9400e982798c125adb2
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-06-01 17:13:09 +0700

    fix pilih paket

--- Layanan/resources/views/Simpanan/daftar-t-sarjana.blade.php
+++ Layanan/resources/views/Simpanan/daftar-t-sarjana.blade.php
@@ -38,62 +38,12 @@
                                 <label for="paket">Pilih Paket:</label>
                                 <select name="paket" id="paket" required="required" class="form-control" onchange="showDetails(this)">
                                     <option value="" disabled selected>Pilih Paket</option>
-                                    <option value="paket1">Paket 1</option>
-                                    <option value="paket2">Paket 2</option>
-                                    <option value="paket3">Paket 3</option>
+                                    <option value="paket1">Paket 1 - 15.000/Bulan selama 4 tahun</option>
+                                    <option value="paket2">Paket 2 - 20.000/Bulan selama 3 tahun</option>
+                                    <option value="paket3">Paket 3 - 30.000/Bulan selama 2 tahun</option>
                                 </select>
-                            </div>
-                            <div id="details-container" class="details-container">
-                                <div id="paket1-details" class="details">Detail Paket 1</div>
-                                <div id="paket2-details" class="details">Detail Paket 2</div>
-                                <div id="paket3-details" class="details">Detail Paket 3</div>
-                            </div>
+                            </div>  
                         </div>
-
-                        <style>
-                            .details-container {
-                                display: none;
-                            }
-
-                            .details {
-                                display: none;
-                                background-color: #f9f9f9;
-                                padding: 10px;
-                                margin-top: 10px;
-                            }
-                        </style>
-
-                        <script>
-                            function showDetails(selectElement) {
-                                var selectedValue = selectElement.value;
-                                var detailsContainer = document.getElementById('details-container');
-                                var detailsElements = detailsContainer.getElementsByClassName('details');
-
-                                // Hide all detail elements
-                                for (var i = 0; i < detailsElements.length; i++) {
-                                    detailsElements[i].style.display = 'none';
-                                }
-
-                                // Show the selected detail element
-                                if (selectedValue !== '') {
-                                    var selectedDetailsElement = document.getElementById(selectedValue + '-details');
-                                    selectedDetailsElement.style.display = 'block';
-                                }
-                            }
-
-                            // Show details when hovering over select element
-                            var selectElement = document.getElementById('paket');
-                            selectElement.addEventListener('mouseover', function () {
-                                showDetails(this);
-                            });
-
-                            // Hide details when not hovering over select element
-                            selectElement.addEventListener('mouseout', function () {
-                                var detailsContainer = document.getElementById('details-container');
-                                detailsContainer.style.display = 'none';
-                            });
-                        </script>
-
 
                         <div class="ts1form-group">
                                 <div class="input-box">
