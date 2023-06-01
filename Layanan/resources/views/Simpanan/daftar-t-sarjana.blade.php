718c33d5b925f2163824e39722dee08da09eb0e2
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-06-01 16:43:42 +0700

    tmbh detail paket

--- Layanan/resources/views/Simpanan/daftar-t-sarjana.blade.php
+++ Layanan/resources/views/Simpanan/daftar-t-sarjana.blade.php
@@ -23,47 +23,30 @@
                         <div class="ts1form-group">
                             <div class="input-box">
                                 <label for="paket">Pilih Paket:</label>
-                                <select name="paket" id="paket" required="required" class="form-control" onchange="showDetails(this)">
-                                    <option value="">Pilih Paket</option>
-                                    <option value="paket1">Paket 1</option>
-                                    <option value="paket2">Paket 2</option>
-                                    <option value="paket3">Paket 3</option>
+                                <select name="paket" id="paket" required="required" class="form-control">
+                                    <option value="" disabled selected>Pilih Paket</option>
+                                    <option value="paket1" onmouseover="showDetails('paket1')">Paket 1</option>
+                                    <option value="paket2" onmouseover="showDetails('paket2')">Paket 2</option>
+                                    <option value="paket3" onmouseover="showDetails('paket3')">Paket 3</option>
                                 </select>
                             </div>
                         </div>
 
-                        <div id="detailPaket" style="display: none;">
+                        <div id="detailPaket">
                             <!-- Detail paket akan ditampilkan di sini -->
                         </div>
 
                         <script>
-                            function showDetails(selectElement) {
+                            function showDetails(paket) {
                                 var detailDiv = document.getElementById('detailPaket');
                                 detailDiv.innerHTML = '';
 
-                                var selectedOption = selectElement.options[selectElement.selectedIndex];
-                                var selectedValue = selectedOption.value;
-
-                                if (selectedValue !== '') {
-                                    // Ganti dengan logika Anda untuk menampilkan detail paket berdasarkan nilai yang dipilih
-                                    var detailText = 'Detail Paket ' + selectedValue;
-                                    detailDiv.innerHTML = detailText;
-                                }
+                                // Ganti dengan logika Anda untuk menampilkan detail paket berdasarkan nilai yang dipilih
+                                var detailText = 'Detail Paket ' + paket;
+                                detailDiv.innerHTML = detailText;
                             }
-
-                            var selectElement = document.getElementById('paket');
-                            selectElement.onmouseover = function() {
-                                var detailDiv = document.getElementById('detailPaket');
-                                if (detailDiv.innerHTML !== '') {
-                                    detailDiv.style.display = 'block';
-                                }
-                            };
-
-                            selectElement.onmouseout = function() {
-                                var detailDiv = document.getElementById('detailPaket');
-                                detailDiv.style.display = 'none';
-                            };
                         </script>
+
 
 
                         <div class="ts1form-group">
