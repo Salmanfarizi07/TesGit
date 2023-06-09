7ebc38dc819132e3b71f70e97e03c9d398ad0a1c
Muhammad Zano Afnan H <zanoafnan@students.unnes.ac.id>
2023-06-09 17:07:11 +0700

    update view buku

--- Layanan/resources/views/buku_zano.blade.php
+++ Layanan/resources/views/buku_zano.blade.php
@@ -41,6 +41,59 @@
 	</table>
 	<button class="tambah"><a href="/tambahbuku_zano">Tambah Buku</a></button>
 </body>
+<style>
+		.tabel {
+			border-collapse: collapse;
+			margin: 25px 0;
+			font-size: 0.9em;
+			font-family: sans-serif;
+			min-width: 400px;
+			}
 
+		.tabel thead tr {
+			background-color: #009879;
+			color: #ffffff;
+			text-align: left;
+			}
+
+		.tabel th,
+		.tabel td {
+			padding: 12px 15px;
+		}
+
+		.tabel tbody tr {
+    		border-bottom: 1px solid #dddddd;
+		}
+
+		.tabel tbody tr:nth-of-type(even) {
+			background-color: #f3f3f3;
+		}
+
+		.tabel tbody tr:last-of-type {
+			border-bottom: 2px solid #009879;
+		}
+
+		.tabel tbody tr.active-row {
+			font-weight: bold;
+			color: #009879;
+		}
+		
+		.tambah {
+			background-color: #91c495; 
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+
+		.h3 {
+			font-weight: bold;
+			font-family: sans-serif;
+		}
+
+	</style>
 </html>
 
