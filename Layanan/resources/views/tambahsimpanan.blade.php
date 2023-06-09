5d58f7af09361b9b729b283b972be94b7f40d3fa
DESKTOP-V6RNP48\salma <salmanfarizi.as@gmail.com>
2023-06-09 14:44:20 +0700

    add tambahsimpanan

--- Layanan/resources/views/tambahsimpanan.blade.php
+++ Layanan/resources/views/tambahsimpanan.blade.php
@@ -1,26 +1,34 @@
 @include('layouts.app-master')
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Tambah Simpanan</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
- 
-		<h2>Data Simpanan</h3>
- 
-	<a href="/simpanan"> Kembali</a>
-	
-	<br/>
-	<br/>
- 
-	<form action="/simpanan/input" method="post">
-		{{csrf_field()}}
-		ID User <input type="number" name="id_user" required="required"> <br/>
-		Jenis Simpanan <input type="text" name="jenis_simpanan" required="required"> <br/>
-		Tanggal Masuk <input type="date" name="tgl_awal" required="required"> <br/>
-		Jumlah Simpanan <input type="number" name="jumlah_simpanan" required="required"> <br/>
-		<input type="submit" value="Simpan Data">
+<h2 class="judul">Tambah Simpanan</h2>
+    <a href="/simpanan" class="kembali">Kembali</a>
+    <form action="/simpanan/input" method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="id_user">ID User</label>
+            <input type="number" name="id_user" required="required">
+        </div>
+        <div class="form-group">
+            <label for="jenis_simpanan">Jenis Simpanan</label>
+            <input type="text" name="jenis_simpanan" required="required">
+        </div>
+        <div class="form-group">
+            <label for="tgl_awal">Tanggal Masuk</label>
+            <input type="date" name="tgl_awal" required="required">
+        </div>
+        <div class="form-group">
+            <label for="jumlah_simpanan">Jumlah Simpanan</label>
+            <input type="number" name="jumlah_simpanan" required="required">
+        </div>
+        <input type="submit" value="Simpan Data" class="simpan">
 	</form>
- 
 </body>
 </html>
