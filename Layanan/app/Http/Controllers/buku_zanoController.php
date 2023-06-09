6bf8e1c2c1503007601354db7c74d70ef0c2127f
Muhammad Zano Afnan H <zanoafnan@students.unnes.ac.id>
2023-06-09 15:54:37 +0700

    add buku

---
+++ Layanan/app/Http/Controllers/buku_zanoController.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use app\User;
+use app\Role;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class buku_zanoController extends Controller
+{
+    public function index()
+    {
+        // mengambil data dari table Saham
+    	$buku_zano = DB::table('buku_zano')->get();
+        
+    	// mengirim data Saham ke view index
+    	return view('buku_zano',['buku_zano' => $buku_zano]);
+        
+ 
+    }
+    
+    // method untuk menampilkan view form tambah buku_zano
+    public function tambah()
+    {
+        
+        // memanggil view tambah
+        return view('tambah');
+   
+    }
+
+    // method untuk insert data ke table buku_zano
+    public function store(Request $request)
+    {
+        // insert data ke table buku_zano
+        DB::table('buku_zano')->insert([
+            'Id_buku_zano' => $request->Id_buku_zano,
+            'Id_User' => $request->Id_User,
+            'Jenis_buku_zano' => $request->Jenis_buku_zano,
+            'Metode_buku_zano' => $request->Metode_buku_zano,
+            'Status_buku_zano' => $request->Status_buku_zano
+        ]);
+        // alihkan halaman ke halaman buku_zano
+        return redirect('/buku_zano');
+    
+    }
+
+    // method untuk edit data buku_zano
+    public function edit($Id_buku_zano)
+    {
+        // mengambil data buku_zano berdasarkan id yang dipilih
+        $buku_zano = DB::table('buku_zano')->where('Id_buku_zano',$Id_buku_zano)->get();
+        // passing data buku_zano yang didapat ke view edit.blade.php
+        return view('editbuku_zano',['buku_zano' => $buku_zano]);
+    
+    }
+
+    // update data buku_zano
+    public function update(Request $request)
+    {
+        // update data buku_zano
+        DB::table('buku_zano')->where('Id_buku_zano',$request->Id_buku_zano)->update([
+            
+            'Jenis_buku_zano' => $request->Jenis_buku_zano,
+            'Metode_buku_zano' => $request->Metode_buku_zano,
+            'Status_buku_zano' => $request->Status_buku_zano
+        ]);
+        // alihkan halaman ke halaman buku_zano
+        return redirect('/buku_zano');
+    }
+
+    // method untuk hapus data buku_zano
+    public function hapus($Id_buku_zano)
+    {
+        // menghapus data buku_zano berdasarkan id yang dipilih
+        DB::table('buku_zano')->where('Id_buku_zano',$Id_buku_zano)->delete();
+            
+        // alihkan halaman ke halaman buku_zano
+        return redirect('/buku_zano');
+    }
+}
