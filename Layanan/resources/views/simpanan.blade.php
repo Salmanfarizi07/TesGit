3777b3dcd618a26e21575c950c79bbefb8efe169
DESKTOP-V6RNP48\salma <salmanfarizi.as@gmail.com>
2023-06-09 14:35:04 +0700

    add

--- Layanan/resources/views/simpanan.blade.php
+++ Layanan/resources/views/simpanan.blade.php
@@ -1,20 +1,17 @@
 @include('layouts.app-master')
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Simpanan</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
- 
-	<h2>Sistem Layanan</h2>
-	<h3>Daftar Simpanan</h3>
- 
-	<a href="/simpanan/tambah"> + Tambah Simpanan Baru</a>
-	
-	<br/>
-	<br/>
- 
-	<table border="1">
+<h3 class="h3">
+	Simpanan
+	</h3>
+<table border="1" class="tabel">
 		<tr>
 			<th>ID Simpanan</th>
 			<th>ID User</th>
@@ -31,6 +28,7 @@
 			<td>{{ $s->tgl_awal }}</td>
 			<td>{{ $s->jumlah_simpanan }}</td>
 			<td>
+			<td>
 				<a href="/simpanan/edit/{{ $s->id_simpanan }}">Edit</a>
 				|
 				<a href="/simpanan/hapus/{{ $s->id_simpanan }}">Hapus</a>
@@ -38,7 +36,62 @@
 		</tr>
 		@endforeach
 	</table>
- 
- 
+	<button class="tambah"><a href="/simpanan/tambah">Tambah Peminjaman</a></button>
+	<style>
+		.tabel {
+			border-collapse: collapse;
+			margin: 25px 0;
+			font-size: 0.9em;
+			font-family: sans-serif;
+			min-width: 400px;
+			}
+
+		.tabel thead tr {
+			background-color: #009879;
+			color: #ffffff;
+			text-align: left;
+			}
+
+		.tabel th,
+		.tabel td {
+			padding: 12px 15px;
+		}
+
+		.tabel tbody tr {
+    		border-bottom: 1px solid #dddddd;
+		}
+
+		.tabel tbody tr:nth-of-type(even) {
+			background-color: #f3f3f3;
+		}
+
+		.tabel tbody tr:last-of-type {
+			border-bottom: 2px solid #009879;
+		}
+
+		.tabel tbody tr.active-row {
+			font-weight: bold;
+			color: #009879;
+		}
+		
+		.tambah {
+			background-color: #91c495; 
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+		}
+
+		.h3 {
+			font-weight: bold;
+			font-family: sans-serif;
+		}
+
+
+	</style>
 </body>
+
 </html>
