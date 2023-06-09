4a8aa172654aa54e68695f080bd55c27c97f19a9
DESKTOP-V6RNP48\salma <salmanfarizi.as@gmail.com>
2023-06-09 16:11:12 +0700

    buku sal

---
+++ Layanan/app/Http/Controllers/BukusalmanController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//import Model "Post
+use App\Models\Buku_Salman;
+
+//return type View
+use Illuminate\View\View;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class BukulalaController extends Controller
+{
+    /**
+     * index
+     *
+     * @return View
+     */
+    
+    public function index(): View
+    {
+        //get posts
+        $Buku_salman = buku_salman::latest()->paginate(5);
+
+        //render view with posts
+        return view('buku_salman.index', compact('Buku_Salman'));
+    }
+
+    
+    
+}
