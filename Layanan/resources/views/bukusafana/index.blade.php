040dc109ec86cd9ebbc8bfc8fb9aa956d20cf05d
LAPTOP-L5EUQFA3\SAFANA SALSABILA <safanasalsabilla10@students.unnes.ac.id>
2023-06-09 16:13:07 +0700

    add index safana

--- Layanan/resources/views/bukusafana/index.blade.php
+++ Layanan/resources/views/bukusafana/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 10 CRUD with Image Upload Example from scratch - ItSolutionStuff.com</h2>
+                <h2>BUKU SAFANA</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('bukusafana.create') }}"> Create New Product</a>
